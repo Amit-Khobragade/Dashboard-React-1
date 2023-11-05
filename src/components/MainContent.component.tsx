9faab38039f5440e8ff6f336bb82d2096ecf341e
Amit Khobragade <amit.khobragade@ecomexperts.io>
2023-11-05 11:31:08 +0530

    Added base structure for the app
    This base structure includes sidebar and main Content.

---
+++ src/components/MainContent.component.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+interface Props {}
+
+function MainContent(props: Props): ReactNode {
+  return <></>;
+}
+
+export default MainContent;
---
+++ src/components/Sidebar.component.tsx
@@ -0,0 +1,9 @@
+import { ReactNode } from "react";
+
+interface Props {}
+
+function Sidebar(props: Props): ReactNode {
+  return <></>;
+}
+
+export default Sidebar;
