0883c2ffaebb9679670b501eba2a38114e20a933
Amit Khobragade <amitkh1102@outlook.com>
2023-12-17 09:14:28 +0530

    Created a sidebar

---
+++ src/globals/global.variables.ts
@@ -0,0 +1,17 @@
+const GlobalVars = {
+  user: {
+    name: "Riya",
+  },
+  routes: {
+    pages: {
+      home: "/home",
+      liveSessions: "/sessions/live",
+      recordedSessions: "/sessions/rec",
+      mockTest: "/test-results",
+      learningMaterial: "/learning",
+      questionPapers: "/question-papers",
+    },
+  },
+};
+
+export default GlobalVars;
